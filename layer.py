93145070f509895afa580fe4970308eecfff8ab2
Mack Rabeau <22nwd3@queensu.ca>
2025-11-27 11:28:21 -0500

    neural network from scratch

---
+++ layer.py
@@ -0,0 +1,16 @@
+
+
+class Layer:
+    def __init__(self):
+        self.input = None
+        self.output = None
+    
+    def forward(self, input):
+
+        pass
+
+    def backward(self, output_gradient, learning_rate):
+        
+        pass
+
+
